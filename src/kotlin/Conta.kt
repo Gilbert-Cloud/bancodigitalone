433ae17a889247e5dd85e06ebee28ad1653f1189
Andrade <gilbertomissionario@yahoo.com.br>
2021-07-23 15:07:09 -0300

    Conta-Criação
    Signed-off-by: Andrade <gilbertomissionario@yahoo.com.br>

---
+++ src/kotlin/Conta.kt
@@ -0,0 +1,21 @@
+package kotlin
+
+import java.math.BigDecimal
+
+class Conta {
+
+    val Agencia: String.
+    val Numero: String.
+    val Saldo: BigDecimal
+    {
+        fun deposito(valor: BigDecimal) {
+
+        }
+
+        fun saque(valor: BigDecimal) {
+
+        }
+
+
+    }
+}
