b96d930283a0c6979a7c68c99d2a2a53d7334817
Andrade <gilbertomissionario@yahoo.com.br>
2021-07-23 09:05:18 -0300

    private set
    Signed-off-by: Andrade <gilbertomissionario@yahoo.com.br>

--- src/kotlin/Pessoa.kt
+++ src/kotlin/Pessoa.kt
@@ -1,19 +1,19 @@
 package Banckdigital
 
 class Pessoa {
-    var nome : String="Gilberto"
-    var cpf  : String= " 956.143.546-20"
+    var nome: String = "Gilberto"
+    var cpf: String = " 956.143.546-20"
+        private set
 
-    inner class Endereco {
+    constructor()
 
-        var rua : String="rua teste"
-    } }
-
+    fun pessoainfo() = "$nome + $cpf "
+}
 fun main() {
 
     val Gilberto = Pessoa()
 
     println(Gilberto.nome)
     println(Gilberto.cpf)
-    println(Gilberto.Endereco().rua)
+    println(Gilberto.pessoainfo())
 }
